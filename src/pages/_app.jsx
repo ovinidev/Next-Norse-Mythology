9f548391b8179573dcbeab7159f938ed55b3c28d
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-04 09:53:43 -0300

    adding icons

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,12 @@
 import { AppProps } from 'next/app';
 import { SwitchContextProvider } from '../context/SwitchContext';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <SwitchContextProvider>
       <Component {...pageProps} />
     </SwitchContextProvider>
   );
 }
-
-export default MyApp;
 
 MyApp.propTypes = AppProps;
